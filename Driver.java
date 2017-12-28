326662a57ed1e00624327e7457b3e6c4a118e0cc
esandine <esandine@stuy.edu>
2017-12-28 11:01:25 -0500

    Matrix multiplication now created

--- Driver.java
+++ Driver.java
@@ -1,7 +1,13 @@
 public class Driver{
     public static void main(String[] args){
-	Mat m = new Mat(2,3);
-	m.randomize();
-	m.printMat();
+	Mat m1 = new Mat(1,2);
+	m1.niceRandomize();
+	m1.printMat();
+	Mat m2 = new Mat(2,1);
+	m2.niceRandomize();
+	m2.printMat();
+	m1.lMult(m2);
+	System.out.println("m2 times m1");
+	m1.printMat();
     }
 }
--- Mat.java
+++ Mat.java
@@ -3,7 +3,7 @@ public class Mat{
     //Variables
     private int rows;
     private int cols;
-    private double[][] data;
+    private float[][] data;
     
     //Mutators
     public void setRows(int r){
@@ -14,12 +14,27 @@ public class Mat{
 	cols = c;
     }
     public void resetData(){
-	data = new double[rows][cols];
+	data = new float[rows][cols];
     }
-    public void setData(int r,int c,double dat){
+    public void setEntry(int r,int c,float dat){
 	data[r][c]=dat;
     }
-
+    public void addEntry(int r, int c, float dat){
+	setEntry(r, c, getEntry(r,c)+dat);
+    }
+    public void multEntry(int r, int c, float s){
+	setEntry(r, c, getEntry(r,c)*c);
+    }
+    public void copyMat(Mat A){
+	setRows(A.getRows());
+	setCols(A.getCols());
+	resetData();
+	for(int i = 0; i < A.getRows(); i++){
+	    for(int j =0; j < A.getCols(); j++){
+		setEntry(i,j,A.getEntry(i,j));
+	    }
+	}
+    }
     //Accessors
     public int getRows(){
 	return rows;
@@ -28,7 +43,7 @@ public class Mat{
     public int getCols(){
 	return cols;
     }
-    public double getData(int r,int c){
+    public float getEntry(int r,int c){
 	return data[r][c];
     }
     
@@ -43,12 +58,20 @@ public class Mat{
 	this(rows, rows);
     }
 
+    public Mat(Mat m){
+	copyMat(m);
+    }
+
+    public Mat(Mat m1, Mat m2){
+	this(mult(m1,m2));
+    }
+
     //print function
     public String toString(){
 	String printstr="";
 	for(int i = 0; i<getRows(); i++){
 	    for(int j = 0; j<getCols(); j++){
-		printstr+=Double.toString(getData(i,j));
+		printstr+=Float.toString(getEntry(i,j));
 		printstr+=" ";
 	    }
 	    printstr+="\n";
@@ -59,12 +82,44 @@ public class Mat{
 	System.out.println(this);
     }
 
-    //randomMatrix
+    //randomize randomizes the matrix
     public void randomize(){
 	for(int i = 0; i<rows; i++){
 	    for(int j=0; j<cols; j++){
-		setData(i,j,Math.random());
+		setEntry(i,j,(float)Math.random());
 	    }
 	}
     }
+
+    //randomize makes matrices that are easily computed by hand
+    public void niceRandomize(){
+	randomize();
+	for(int i = 0; i<rows; i++){
+	    for(int j=0; j<cols; j++){
+		setEntry(i,j,Math.round(getEntry(i,j)*10));
+	    }
+	}
+    }
+
+    //mult multiplied two matrices together
+    public static Mat mult(Mat A, Mat B){
+	Mat ret = new Mat(A.getRows(), B.getCols());
+	for(int i = 0; i < A.getRows(); i++){
+	    for(int j = 0; j < B.getCols(); j++){
+		for(int k = 0; k < A.getCols(); k++){
+		    ret.addEntry(i,j,A.getEntry(i,k)*B.getEntry(k,j));
+		}
+	    }
+	}
+	return ret;
+    }
+
+    public void lMult(Mat A){
+	copyMat(mult(A, this));
+    }
+
+    public void rMult(Mat A){
+	copyMat(mult(this, A));
+    }
+
 }
