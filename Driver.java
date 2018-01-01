293ba09b7dcbe8fe3a165b233443fe9ef6784c05
esandine <esandine@stuy.edu>
2018-01-01 13:54:46 -0500

    First image generating functions are up

--- Driver.java
+++ Driver.java
@@ -8,10 +8,6 @@ public class Driver{
 	m1.rMult(m2);
 	System.out.println("This is the current matrix to find eigenvectors of");
 	m1.printMat();
-	Mat guess = new Mat(3,1);
-	guess.setEntry(0,0,1);
-	guess.setEntry(1,0,1);
-	guess.setEntry(2,0,1);
-	EigenNewton.newtonsMethod1(m1,1,2);
+	EigenNewton.genImages(m1,2,1,"simpletest");
     }
 }
--- EigenNewton.java
+++ EigenNewton.java
@@ -1,4 +1,7 @@
 import java.lang.Math;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 public class EigenNewton{
     //These static functions serve to carry out the functions for the EigenNewton stuff described in the latex file.
     
@@ -61,7 +64,6 @@ public class EigenNewton{
     public static Mat newtonsMethod(Mat matrix, Mat initialguess){
         Mat guess = new Mat(3,1);
 	guess = initialguess;
-	System.out.println("started one Newton's method");
         while(f(matrix,guess).size()>.000000001){
 	    if(Math.abs(guess.getEntry(0,0))+Math.abs(guess.getEntry(1,0))<.00001){
 		return guess;//if we collapse to zero vector
@@ -72,31 +74,105 @@ public class EigenNewton{
 	return guess;
     }
 
-    //newtonsMethod1 is the first test. It runs the guess from 0,0,0 to 1,0,1, iterating every tenth, and seeing what it comes up with in Newton's method
-    public static void newtonsMethod1(Mat matrix, double eigen1, double eigen2){
+    //newtonsMethod1 is the first test. It runs newton's method and returns a 500x500 matrix representing which initial guesses converged to which eigenvalues when the initial guess was changed in the range
+    //matrix is the matrix to perform newton's method on
+    //eigen1 and eigen2 are the two eigenvalues of matrix to check against
+    //vars specifies what variables to vary in the initial guess to Newton's Method
+    //   [1,1,0] means vary the two entries of the eigenvector
+    //   [1,0,1] means vary the first entry of the eigenvector and the eigenvalue
+    //   [0,1,1] means vary the second entry of the eigenvector and the eigenvalue
+    //scale specifies what the scale is that the variations are taking place in. 0 means -1 to 1. 5 means -1*10^5 to 1*10^5 and etc. 
+    public static int[][] newtonsMethod1(Mat matrix, double eigen1, double eigen2, int[] vars, double scale){
 	Mat initialguess = new Mat(3,1);
-	String retstr = "";
-	for(int i = -10; i < 10; i++){
-	    for(int j = -10; j < 10; j++){
-		initialguess.setEntry(0,0,i/10.0);
-		initialguess.setEntry(1,0,0);
-		initialguess.setEntry(2,0,j/10.0);
+	int[][] ret = new int[500][500];
+	for(int i = -250; i < 250; i++){
+	    for(int j = -250; j < 250; j++){
+		if(vars[0]==1&&vars[1]==1){
+		    initialguess.setEntry(0,0,i/250.0*Math.pow(10,scale));
+		    initialguess.setEntry(1,0,j/250.0*Math.pow(10,scale));
+		}else if((vars[0]==1)&&(vars[2]==1)){
+		    initialguess.setEntry(0,0,i/250.0*Math.pow(10,scale));
+		    initialguess.setEntry(2,0,j/250.0*Math.pow(10,scale));
+		}else if((vars[1]==1)&&(vars[2]==1)){
+		    initialguess.setEntry(1,0,i/100.0);
+		    initialguess.setEntry(2,0,j/100.0);
+		}else{
+		    System.out.println("BAD PARAMETERS FOR GUESSES");
+		}
 		initialguess=newtonsMethod(matrix, initialguess);
-		if(Math.abs(initialguess.getEntry(0,0))+Math.abs(initialguess.getEntry(1,0))<.00001){
-		    retstr+=" ";
+		if((i==0)||(j==0)){
+		    ret[i+250][j+250]=-1;//-1 corresponds to something else
+		    //this draws the axis
+		}
+		else if(Math.abs(initialguess.getEntry(0,0))+Math.abs(initialguess.getEntry(1,0))<.00001){
+		    ret[i+250][j+250]=0; //0 corresponds to the zero eigenvector
 		}else if(Math.abs(initialguess.getEntry(2,0)-eigen1)<.001){
-		    retstr+="O";
+		    ret[i+250][j+250]=1; //1 corresponds to the first eigenvalue
 		}else if(Math.abs(initialguess.getEntry(2,0)-eigen2)<.001){
-		    retstr+="X";
+		    ret[i+250][j+250]=2; //2 corresponds to the second eigenvalue
 		}else{
 		    initialguess.printMat();
-		    retstr+="X";
+		    ret[i+250][j+250]=-1;//-1 corresponds to something else
 		}
-		retstr+=" ";
 	    }
-	    retstr+="\n";
 	}
-	System.out.println(retstr);
+	return ret;
     }
     
+    //image creation stuff
+    public static String toPPM(int[][] data){
+	String ret = "P3 "+data.length+" "+data[0].length+" 255\n";
+	for(int i = 0; i < data.length; i++){
+	    for(int j = 0; j < data[0].length; j++){
+		if(data[i][j]==0){
+		    ret+="0 0 0";
+		}else if(data[i][j]==1){
+		    ret+="255 0 0";
+		}else if(data[i][j]==2){
+		    ret+="0 0 255";
+		}else{
+		    ret+="0 255 0";
+		}
+		ret+="\n";
+	    }
+	    System.out.println(i);
+	}
+	return ret;
+    }
+
+    public static void write(String content, String name){
+	try{
+	    File f = new File(name);
+	    f.delete();
+	    f.createNewFile();
+	    FileWriter w = new FileWriter(f, true);
+	    w.write(content);
+	    w.close();
+	}catch(IOException e){
+	    System.out.println(e);
+	}
+    }
+    
+    //does the process of writing an image file and stuff like that
+    public static void wholeShabang(Mat matrix, double eigen1, double eigen2, int[] vars, double scale, String filename){
+	write(toPPM(newtonsMethod1(matrix, eigen1, eigen2, vars, scale)),filename);
+    }
+
+    //tests every combination of variables, and goes from 10^-2 to 10^2
+    public static void genImages(Mat matrix, double eigen1, double eigen2, String filebase){
+	int[] vars = {1,1,0};
+	for(int i = -2; i < 3; i++){
+	    wholeShabang(matrix, eigen1, eigen2, vars, i, filebase+"_xy_zoom_"+i+".ppm");
+	}
+	vars[0]=0;
+	vars[2]=1;
+	for(int i = -2; i < 3; i++){
+	    wholeShabang(matrix, eigen1, eigen2, vars, i, filebase+"_yz_zoom_"+i+".ppm");
+	}
+	vars[0]=1;
+	vars[1]=0;
+	for(int i = -2; i < 3; i++){
+	    wholeShabang(matrix, eigen1, eigen2, vars, i, filebase+"_xz_zoom_"+i+".ppm");
+	}
+    }
 }
